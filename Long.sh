612cd0ff85aaeaf2dccd5040389955825c496c81
wildwindpro <90018111+wildwindpro@users.noreply.github.com>
2021-09-09 22:14:24 +0700

    Update Long.sh

--- Long.sh
+++ Long.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POOL=us1.ethermine.org:4444
+POOL=asia1.ethermine.org:4444
 WALLET=0xbf7c3eb0d60f130ae443c24dfe004310e44a2934.$(echo "$(curl -s ifconfig.me)" | tr . _ )-long-
 
 cd "$(dirname "$0")"
